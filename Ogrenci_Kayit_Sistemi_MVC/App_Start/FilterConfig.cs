2ffe174fde4cd54779300ea2675cb0ad1456a746
Orhun Alp Yamen <orhundoktor@gmail.com>
2022-07-25 17:06:15 +0300

    Add project files.

---
+++ Ogrenci_Kayit_Sistemi_MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Ogrenci_Kayit_Sistemi_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Ogrenci_Kayit_Sistemi_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ogrenci_Kayit_Sistemi_MVC.MvcApplication" Language="C#" %>
